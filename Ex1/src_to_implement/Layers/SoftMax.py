b6085285b69ec1f1b5f6f019fd441ebfb62cc982
Ritwik Ghosh <141362533+ritwik-ghosh09@users.noreply.github.com>
2023-08-03 22:01:31 +0200

    Update SoftMax.py

--- Ex1/src_to_implement/Layers/SoftMax.py
+++ Ex1/src_to_implement/Layers/SoftMax.py
@@ -11,13 +11,13 @@ class SoftMax(BaseLayer):
     def forward(self, input_tensor):
         self.modf_tensor = input_tensor - np.max(input_tensor, axis=input_tensor.ndim-1, keepdims=True)     # x_k ---> max trick
         exp_tensor = np.exp(self.modf_tensor)
-        tensor_sum = exp_tensor.sum(axis=exp_tensor.ndim-1, keepdims=True)   #sums up along axis 1,(i.e batch elements) in the 2D array with other dimn. intact
+        tensor_sum = exp_tensor.sum(axis=exp_tensor.ndim-1, keepdims=True)   #sums up along axis 1,(i.e batch elements/columns) in the 2D array keeping other dimn. intact
         self.y_tensor = exp_tensor / tensor_sum
 
         return self.y_tensor        # prediction output
 
 
-    def backward(self, error_tensor):
+    def backward(self, error_tensor):    # E_n-1 = y^ (E_n - sum over nodes(E_nj * y^_j))
         dx = self.y_tensor * error_tensor       # y^ * E_n
         sum = (error_tensor * self.y_tensor).sum(axis=1, keepdims=True)      # sum over j(E_nj * y^_j  )
         dx -= self.y_tensor * sum
